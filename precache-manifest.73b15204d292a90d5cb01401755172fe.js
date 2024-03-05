58da29e5b996fd0b5b7e1a3acafcd38c3affd559
Ryan Feng <ryan.feng@bondexchange.com.au>
2024-03-06 00:43:59 +1100

    Updates

---
+++ precache-manifest.73b15204d292a90d5cb01401755172fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b30d5c6941b02024259123d1fa701107",
+    "url": "/transaction-table/index.html"
+  },
+  {
+    "revision": "d006eb208655c1b917ee",
+    "url": "/transaction-table/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9ed47afa0a632fb546ed",
+    "url": "/transaction-table/static/js/2.2a092bdb.chunk.js"
+  },
+  {
+    "revision": "d006eb208655c1b917ee",
+    "url": "/transaction-table/static/js/main.698b677f.chunk.js"
+  },
+  {
+    "revision": "e195f37db5fcdf25f898",
+    "url": "/transaction-table/static/js/runtime~main.521fc8c8.js"
+  }
+]);
